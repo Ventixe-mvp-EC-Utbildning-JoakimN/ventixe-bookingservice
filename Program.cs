6b547b5b9b21a3aa1a944238372985e4bb433cd1
Joakim N <92833271+Echoinstrum@users.noreply.github.com>
2025-05-22 14:30:15 +0200

    Model and database setup

--- Program.cs
+++ Program.cs
@@ -1,17 +1,31 @@
+using Microsoft.EntityFrameworkCore;
+using Ventixe.BookingService.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 
+builder.Services.AddDbContext<BookingDbContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowFrontend", policy =>
+    {
+        policy.WithOrigins("http://localhost:5173")
+        .AllowAnyHeader()
+        .AllowAnyMethod();
+    });
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
+app.UseCors("AllowFrontend");
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
