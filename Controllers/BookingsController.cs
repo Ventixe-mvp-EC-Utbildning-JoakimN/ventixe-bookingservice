bb20ff170fefa98b74823826e29c30415a899b78
Joakim N <92833271+Echoinstrum@users.noreply.github.com>
2025-05-26 13:01:00 +0200

    setup booking service and bbooking controller with endpoints done

---
+++ Controllers/BookingsController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Ventixe.BookingService.Data;
+using Ventixe.BookingService.Models;
+using BookingServiceClass = Ventixe.BookingService.Services.BookingService;
+
+namespace Ventixe.BookingService.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BookingsController : Controller
+    {
+        private readonly BookingServiceClass _bookingService;
+
+        public BookingsController(BookingServiceClass bookingService)
+        {
+            _bookingService = bookingService;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Booking>> GetAll()
+        {
+            return Ok(_bookingService.GetAll());
+        }
+
+        [HttpPost]
+        public ActionResult<Booking> Create([FromBody] Booking booking)
+        {
+            var saved = _bookingService.Create(booking);
+            return CreatedAtAction(nameof(GetAll), new { id = saved.Id }, saved);
+        }
+    }
+}
+
---
+++ Migrations/20250526083232_Init.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ventixe.BookingService.Migrations
+{
+    /// <inheritdoc />
+    public partial class Init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Bookings",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    EventId = table.Column<int>(type: "int", nullable: false),
+                    BookedAt = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Bookings", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Bookings");
+        }
+    }
+}
